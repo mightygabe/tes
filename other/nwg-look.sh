34a6f824a3e26d23c5eca5b9a5c935a292e0b32e
mightygabe <BSKShadow20000@proton.me>
2024-07-17 08:09:41 +0530

    Update nwg-look.sh

--- other/nwg-look.sh
+++ other/nwg-look.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-sudo apt install golang libgtk-3-dev libcairo2-dev libglib2.0-bin
+sudo apt install -y golang libgtk-3-dev libcairo2-dev libglib2.0-bin jq
 
 cd ~/Downloads
-wget https://github.com/nwg-piotr/nwg-look/archive/refs/tags/v0.2.7.zip
-unzip v0.2.7.zip
-cd nwg-look-0.2.7
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/nwg-piotr/nwg-look/releases/latest | jq -r '.tag_name')
+wget https://github.com/nwg-piotr/nwg-look/archive/refs/tags/${LATEST_RELEASE}.zip -O nwg-look-latest.zip
+unzip nwg-look-latest.zip
+cd nwg-look-${LATEST_RELEASE#v}
 make build
 sudo make install
 
 cd ..
-rm -rf nwg-look-0.2.7
-rm -rf v0.2.7.zip
+rm -rf nwg-look-${LATEST_RELEASE#v}
+rm -rf nwg-look-latest.zip
